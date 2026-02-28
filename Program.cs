9072bf877a3678650a9087d91de353473afab6f5
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 14:07:43 -0600

    Programa creado!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Mostrar una tabla donde las filas sean del 1 al 4 y las columnas del 1 al 4.
