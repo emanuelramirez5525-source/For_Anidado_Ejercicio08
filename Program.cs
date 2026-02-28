9df8deb246e06d951b0f0983e1f05b26da627af9
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 14:31:01 -0600

    Logica aplica y funcionando!!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
 ﻿// Mostrar una tabla donde las filas sean del 1 al 4 y las columnas del 1 al 4.
+
+
+
+for (int i = 1; i <= 4; i++)
+{
+    
+    for (int j = 1; j <=4; j++)
+    {
+        Console.Write(j + " ");
+    }
+    Console.WriteLine();
+}
